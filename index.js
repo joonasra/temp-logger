5c73c3d432dd53df53bbae57d7253bb939f32475
joonasra <joonas.j.rantanen@gmail.com>
2019-02-19 14:26:07 +0200

    login and users routes added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -16,6 +18,9 @@ mongoose
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'))
+
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
