529e4f3a574bf42bca3de5c16bda529887a15f79
joonasra <joonas.j.rantanen@gmail.com>
2019-02-19 15:17:33 +0200

    added login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return <div className="App">hello world</div>
-  }
+const App = () => {
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="login">
+          <form className="form-signin">
+            <label htmlFor="inputUsername">Käyttäjänimi</label>
+            <input
+              id="inputUsername"
+              className="form-control"
+              type="text"
+              value={username}
+              onChange={() => setUsername(username)}
+              required
+              autoFocus
+            />
+            <label htmlFor="inputPassword">Salasana</label>
+            <input
+              id="inputPassword"
+              className="form-control"
+              type="password"
+              value={password}
+              onChange={() => setPassword(password)}
+              required
+            />
+            <button className="btn btn-md btn-primary btn-block" type="submit">
+              Kirjaudu
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
